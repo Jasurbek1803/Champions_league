38b602dd5e1f22e269daf04c876e86a222e3f6f0
Jasurbek1803 <137008581+Jasurbek1803@users.noreply.github.com>
2024-02-08 20:28:18 +0500

    urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+from django.conf.urls.static import static
+from conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
